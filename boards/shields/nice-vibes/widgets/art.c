955f2004f159d0c8f2e9f82489593f9ed92b00d1
xvargr <contact@xvargr.dev>
2026-04-17 21:47:20 +0800

    prepare blank slate for art

--- boards/shields/nice-vibes/widgets/art.c
+++ boards/shields/nice-vibes/widgets/art.c
@@ -12,11 +12,11 @@
 #define LV_ATTRIBUTE_MEM_ALIGN
 #endif
 
-#ifndef LV_ATTRIBUTE_IMG_HAMMERBEAM1
-#define LV_ATTRIBUTE_IMG_HAMMERBEAM1
+#ifndef LV_ATTRIBUTE_IMG_NIX1
+#define LV_ATTRIBUTE_IMG_NIX1
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_HAMMERBEAM1 uint8_t hammerbeam1_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_NIX1 uint8_t nix1_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
         0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
@@ -27,22 +27,22 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_HAMMERBEA
 
 };
 
-const lv_img_dsc_t hammerbeam1 = {
+const lv_img_dsc_t nix1 = {
   .header.cf = LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 140,
   .header.h = 68,
   .data_size = 1232,
-  .data = hammerbeam1_map,
+  .data = nix1_map,
 };
 
 
-#ifndef LV_ATTRIBUTE_IMG_HAMMERBEAM2
-#define LV_ATTRIBUTE_IMG_HAMMERBEAM2
+#ifndef LV_ATTRIBUTE_IMG_NIX2
+#define LV_ATTRIBUTE_IMG_NIX2
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_HAMMERBEAM2 uint8_t hammerbeam2_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST  LV_ATTRIBUTE_IMG_NIX2 uint8_t nix2_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
         0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
@@ -53,12 +53,12 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_HAMMERBEA
 
 };
 
-const lv_img_dsc_t hammerbeam2 = {
+const lv_img_dsc_t nix2 = {
   .header.cf = LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 140,
   .header.h = 68,
   .data_size = 1232,
-  .data = hammerbeam2_map,
+  .data = nix2_map,
 };
